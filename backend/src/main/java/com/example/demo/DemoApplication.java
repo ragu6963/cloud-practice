22e9f9114b3f075a6b0b64d96bfce0c6770b9036
ragu6936 <bhj1684@naver.com>
2024-11-19 20:38:18 +0900

    environment-variable backend

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,14 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
--- backend/src/main/java/com/example/demo/controller/HelloController.java
+++ backend/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,25 @@
 package com.example.demo.controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @RestController
 public class HelloController {
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
     @GetMapping("/")
     public String hello() {
-        return "!!!";
+        StringBuilder sb = new StringBuilder();
+        sb.append("<p>");
+        sb.append("DB User: ").append(dbUser);
+        sb.append("<br/>");
+        sb.append("DB Password: ").append(dbPassword);
+        sb.append("</p>");
+        return sb.toString();
     }
 }
