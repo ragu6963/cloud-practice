c9aeea9908a504fe256bda01a8c8afe6932a0e97
ragu6936 <bhj1684@naver.com>
2024-11-29 15:17:27 +0900

    환경 변수 실습 베이스 프로젝트

--- backend/src/main/java/com/example/demo/controller/HelloController.java
+++ backend/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,17 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class HelloController {
-	@Value("${MESSAGE}")
-	private String message;
-
 	@GetMapping("/")
 	public String hello() {
-		return message;
+		return "Spring Boot 환경 변수를 불러와서 응답하시오.";
 	}
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,15 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 function App() {
-  const vite_message = import.meta.env.VITE_MESSAGE;
-  const [spring_message, setSpringMessage] = useState("");
-  const url = "http://localhost:8080/";
+  const URL = "http://localhost:8080/";
+  const vite_message = "Vite 환경 변수를 불러와서 화면에 출력하시오.";
+  ("Spring Boot 환경 변수를 요청해서 화면에 출력하시오.");
+  const [spring_message, setSpringMessage] = useState();
+
+  // Spring Boot 환경 변수 요청 함수를 완성하시오.
+  async function fetchMessage() {}
+
   useEffect(() => {
-    async function fetchMessage() {
-      const response = await axios.get(url);
-      setSpringMessage(response.data);
-    }
     fetchMessage();
   }, []);
 
