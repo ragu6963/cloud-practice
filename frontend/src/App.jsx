6d6e0fe03e9a0899e1020a953cb688f701107f29
ragu6936 <bhj1684@naver.com>
2024-11-29 15:30:06 +0900

    환경 변수 실습 베이스 프로젝트

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,10 @@ import { useState, useEffect } from "react";
 function App() {
   const URL = "http://localhost:8080/";
   const vite_message = "Vite 환경 변수를 불러와서 화면에 출력하시오.";
-  ("Spring Boot 환경 변수를 요청해서 화면에 출력하시오.");
-  const [spring_message, setSpringMessage] = useState();
+
+  const [spring_message, setSpringMessage] = useState(
+    "Spring Boot 환경 변수를 요청해서 화면에 출력하시오."
+  );
 
   // Spring Boot 환경 변수 요청 함수를 완성하시오.
   async function fetchMessage() {}
